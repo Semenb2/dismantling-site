971b720098e3a48c2a34a1fb2a6191cd5ddfe450
Semen_b2 <semenberezhnov1@gmail.com>
2025-08-23 00:39:15 +0300

    Version 3.0

--- pastor_site/settings.py
+++ pastor_site/settings.py
@@ -108,8 +108,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
 
+# Где искать статику в режиме разработки
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# Куда собирать статику для продакшена
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
